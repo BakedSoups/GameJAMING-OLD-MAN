57f1ec64d5b146226f9bae060183f4b367a9da68
Pixalia <davidcalsp@gmail.com>
2023-11-05 12:30:14 -0800

    Added ui and powerup objects

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -22,15 +22,26 @@ var gravity = 1200
 
 var hook = preload("res://GameObjects/grappling_hook.tscn")
 var h
+var hooked = false
+
+var wing = preload("res://GameObjects/wings.tscn")
+var w
+var winged = false
 
 func shoot():
-	if Input.is_key_pressed(KEY_Z) and not dashing and not hasFired:
+	if Input.is_key_pressed(KEY_Z) and not dashing and not hasFired and hooked:
 		hasFired = true
 		velocity.y = 0
 		dashing = true
 		h = hook.instantiate()
 		h.position = Vector2(position.x, position.y - 40)
 		get_parent().add_child(h)
+		
+func bodyFun():
+	if winged:
+		w = wing.instantiate()
+		w.position = Vector2(position.x, position.y)
+		add_child(w)
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_left") and not dashing:
@@ -64,10 +75,9 @@ func _physics_process(delta):
 			elif climbing:
 				ladderJump = true;
 				velocity.y = JUMP_VELOCITY
-			elif doubleJump:
+			elif doubleJump and winged:
 				doubleJump = false
 				velocity.y = JUMP_VELOCITY
-				
 		var direction = Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED
