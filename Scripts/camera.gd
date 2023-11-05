304117a53fdd6cbd7677a52bed093f06750a90fa
Pixalia <davidcalsp@gmail.com>
2023-11-04 17:24:05 -0700

    Updated camera again
    Camera bounds betters

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,14 +1,16 @@
 extends Camera2D
 
+var py = position.y
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_zoom(get_zoom() - Vector2(0.25, 0.25))
 
 func _physics_process(delta):
 	var p = get_parent().get_node("Player")
-	if abs(p.position.y - position.y) > 400:
+	if abs(p.position.y - position.y) > 1200:
 		if (p.position.y < position.y):
-			position.y -= 800
+			position.y -= 2200
 		else:
-			position.y += 800
+			position.y += 2200
 	#comment ehre
